6b8d83c835ce48e778593cbbf276002c182494b1
rcout328 <solovpxoffical@gmail.com>
2024-05-01 21:31:59 +0530

    c

--- src/app/home/Part2.jsx
+++ src/app/home/Part2.jsx
@@ -2,7 +2,7 @@ export default function Part2({ title, stitle }) {
   return (
     <div className="flex flex-row w-[20rem] h-[5rem] bg-gray rounded-lg">
       <div className="flex flex-col ml-7 mt-7">
-        <h1 className="text-xl font-bold">For Men's</h1>
+        <h1 className="text-xl font-bold">For Mens</h1>
         <h1 className="text-xs">Starting at $24</h1>
       </div>
       <h1 className="ml-[6rem] mt-8">Shop Now</h1>
