59ada34b8b217a15aa18430326350ffbb1ef0d60
rcout328 <solovpxoffical@gmail.com>
2024-05-08 22:56:09 +0530

    c

--- src/app/Part1.jsx
+++ src/app/Part1.jsx
@@ -1,7 +1,7 @@
 export default function Part1({ title, stitle }) {
   return (
     <div className="flex flex-col">
-      <h1 className="text-[1rem] font-bold">{title}</h1>
+      <h1 className="text-[1.1rem] font-bold">{title}</h1>
       <h1 className="text-xs">{stitle}</h1>
     </div>
   );
